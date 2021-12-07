4b1dbb4324b9f1df7e24dc4cfe95a15a18a4da21
raninder dhillon <raninderdhillon@gmail.com>
2021-12-07 18:44:31 -0500

    DayListItems styled based on props.selecetd and props.spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,13 +1,20 @@
 import React from "react";
 import classNames from "classnames";
-import "components/Button.scss";
+import "components/DayListItem.scss";
+import { PROPERTY_TYPES } from "@babel/types";
 
 export default function DayListItem(props) {
-   
+   const {name, spots, setDay} = props;
+	 
+	 let dayClass = classNames("day-list__item", {
+		"day-list__item--selected": props.selected,
+		"day-list__item--full": props.spots === 0
+	});
+
    return (
-		<li>
-		<h2 className="text--regular">Day Name</h2> 
-		<h3 className="text--light">X spots remaining</h3>
+		<li className={dayClass} onClick={() => setDay(name)}>
+		<h2 className="text--regular">Day Name: {name}</h2> 
+		<h3 className="text--light">{spots} spots remaining </h3>
 	</li>
    );
 }
