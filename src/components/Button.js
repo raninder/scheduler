cc953a339496c5f0230eb43159938e7872249935
raninder dhillon <raninderdhillon@gmail.com>
2021-12-07 16:18:33 -0500

    classnames library and functionalioty added

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,7 +9,6 @@ export default function Button(props) {
     });
    return (
       <button
-      
          className={buttonClass}
          onClick={props.onClick}
          disabled={props.disabled}
