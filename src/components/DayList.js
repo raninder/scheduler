9e04a3d18b75542748569e1adf860aacc9789d8e
raninder dhillon <raninderdhillon@gmail.com>
2021-12-07 22:40:31 -0500

    setDay function revised

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,15 +2,17 @@ import React from "react";
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
-	const {days} = props;
+	console.log("props1", props);
+	const { days, setDay, day } = props;
+	console.log("days", days);
 	//const arrDays = days.map(dayItem =><DayListItem key={dayItem.id} {...dayitem} />);
-	const arrDays = days.map(dayItem =><DayListItem key={dayItem.id} name={dayItem.name} spots={dayItem.spots} selected={dayItem.name === props.day} setDay={dayItem.setDay}/>);
+	const arrDays = days.map(dayItem => <DayListItem key={dayItem.id} name={dayItem.name} spots={dayItem.spots} selected={dayItem.name === day} setDay={setDay} />);
 	return (
 		<ul>
 			<li>
-				{arrDays} 
+				{arrDays}
 			</li>
-			</ul>
-	
-	); 
+		</ul>
+
+	);
 }
