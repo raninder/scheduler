f67208a909bb3a650138a0a00364f3b9fe98dc2f
raninder dhillon <raninderdhillon@gmail.com>
2021-12-08 17:16:40 -0500

    InterviewerList created with InterviewerlistItem as children

---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import InterviewListItem from "components/InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+const {interviewers} = props;
+
+const arrInterviewer = interviewers.map((item) => <InterviewListItem key = {item.id} name = {item.name} avatar = {item.avatar} selected = {item.id=== props.interviewer} setInterviewer = {()=>props.setInterviewer(item.id)} />);
+
+	return (
+		<section className="interviewers">
+			<h4 className="interviewers__header text--light">Interviewer</h4>
+			<ul className="interviewers__list">
+				{arrInterviewer}
+			</ul>
+		</section>
+	);
+}
