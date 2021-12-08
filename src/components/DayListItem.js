88460772dee5ba831b40e2dacc5b7e3cfde41c70
raninder dhillon <raninderdhillon@gmail.com>
2021-12-07 19:33:38 -0500

    format function added for 0 or 1 spots in DayListitem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,13 +8,27 @@ export default function DayListItem(props) {
 	 
 	 let dayClass = classNames("day-list__item", {
 		"day-list__item--selected": props.selected,
-		"day-list__item--full": props.spots === 0
+		"day-list__item--full": spots === 0
 	});
+	const formatSpots = () =>{
+		if(spots === 0){
+			console.log(spots);
+		return "no spots remaining";
+		}
+		else if(spots === 1) {
+			console.log(spots);
+			return  "1 spot remaining";
+		}
+		else {
+			console.log(spots);
+			return`${spots} spots remaining`;
+		}
+	}
 
    return (
 		<li className={dayClass} onClick={() => setDay(name)}>
 		<h2 className="text--regular">Day Name: {name}</h2> 
-		<h3 className="text--light">{spots} spots remaining </h3>
+		<h3 className="text--light">{formatSpots()} </h3>
 	</li>
    );
 }
