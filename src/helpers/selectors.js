debd4558300d2a486793ac72b5b60912224715d5
raninder dhillon <raninderdhillon@gmail.com>
2021-12-10 10:40:00 -0500

    filtering appointments for a particular day by defining a helper function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,12 @@
+export default function getAppointmentsForDay(state, day) {
+	const result = [];
+  const filteredDay = state.days.filter(item => item.name === day);
+   if (filteredDay[0]) {
+    filteredDay[0].appointments.map(id => {
+      result.push(state.appointments[id])
+    });
+  }
+  return result;
+};
+
+
