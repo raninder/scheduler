4facff9e594150c8639e58a1eaa349bcf3b5d187
raninder dhillon <raninderdhillon@gmail.com>
2021-12-10 17:14:56 -0500

    helper function for getting interviewer details for an interview

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,12 +1,27 @@
-export default function getAppointmentsForDay(state, day) {
-	const result = [];
+export function getAppointmentsForDay(state, day) {
+  const result = [];
   const filteredDay = state.days.filter(item => item.name === day);
-   if (filteredDay[0]) {
+  if (filteredDay[0]) {
     filteredDay[0].appointments.map(id => {
       result.push(state.appointments[id])
     });
   }
   return result;
-};
+}
 
+export function getInterview(state, interview) {
+  let newObj = {};
+  if (interview !== null) {
+    newObj["student"] = interview.student;
+    for (let key in state.interviewers) {
+      if (key == interview.interviewer) {
+        newObj["interviewer"] = state.interviewers[key];
+      }
+    }
+    return newObj;
+  }
+  else {
+    return null;
+  }
 
+}
