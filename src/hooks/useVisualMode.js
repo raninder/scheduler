b3ea30d82d99ca5c0ab92f1ba29f3f39e2f2cb82
raninder dhillon <raninderdhillon@gmail.com>
2021-12-14 11:19:25 -0500

    avoiding stale state by using previous state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,20 +4,21 @@ export default function useVisualMode(initial) {
 	const [history, setHistory] = useState([initial]);
 
 	const transition = function (newmode, replace) {
-		const newHistory = [...history];
 		if (replace) {
-			newHistory.pop();
+		//to avoid stale state, instead of [...history.pop()];
+		return setHistory(prev => [mode, ...prev.slice(1)]);
 		}
-		setHistory([...newHistory, newmode]);
+		return setHistory(prev => [mode, ...prev]);
 		
 	}
 	const back = function () {
 		if (history.length < 2) {
 			return;
 		}
-		const newHistory = [...history];
-		newHistory.pop();
-		setHistory(newHistory);
+		// const newHistory = [...history];
+		// newHistory.pop();
+		// setHistory(newHistory);
+		return setHistory(prev => [...prev.slice(1)]);
 	}
 	const mode = history[history.length - 1];
 	return (
