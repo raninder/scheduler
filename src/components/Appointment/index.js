ff06c4c38f1817bbd4a1dc644174b742c5215d83
raninder dhillon <raninderdhillon@gmail.com>
2021-12-13 19:45:51 -0500

    save function defined with call to bookInterview and Status component Saving mode added

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Form from "components/Appointment/Form";
+import Status from "components/Appointment/Status";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
@@ -11,11 +12,23 @@ export default function Appointment(props) {
 	const EMPTY = "EMPTY";
 	const SHOW = "SHOW";
 	const CREATE = "CREATE";
-
+	const SAVING = "SAVING";
 	const { mode, transition, back } = useVisualMode(
 		props.interview ? SHOW : EMPTY
 	);
 
+	function save(name, interviewer) {
+		const interview = {
+			student: name,
+			interviewer
+		};
+		transition(SAVING);
+
+		props.bookInterview(props.id, interview)
+			.then((res) =>
+				transition(SHOW))
+
+	}
 	return (
 		<Fragment>
 			<Header time={props.time} />
@@ -33,9 +46,12 @@ export default function Appointment(props) {
 					: <Empty />} */}
 				{mode === CREATE &&
 					<Form onCancel={() => back()}
+						onSave={save}
 						interviewers={props.interviewers}
 					/>
 				}
+
+				{mode === SAVING && <Status />}
 
 			</article>
 		</Fragment>
