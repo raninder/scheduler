98bcdc14ad523c34352832d37b34bfcfe4047db1
raninder dhillon <raninderdhillon@gmail.com>
2021-12-16 14:58:47 -0500

    edit appointment modified

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -35,20 +35,6 @@ describe("Appointments", () => {
 		cy.contains(".appointment__card--show", "Tori Malcolm");
 	});
 
-	it("should edit an interview", () => {
-		cy.get("[alt=Add]")
-			.first()
-			.click();
-
-		cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-		cy.get('[alt="Sylvia Palmer"]').click();
-
-		cy.contains("Save").click();
-
-		cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-		cy.contains(".appointment__card--show", "Sylvia Palmer");
-	});
-
 
 	it("should cancel an interview", () => {
 		cy.get("[alt=Delete]")
