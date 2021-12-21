d0e25de1363fa506940acc9457311fc2cce3f518
raninder dhillon <raninderdhillon@gmail.com>
2021-12-20 21:57:34 -0500

    setSpots function added and functions converted to arrow functions

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,17 +1,19 @@
 // get Appointments for day
-export function getAppointmentsForDay(state, day) {
+export const getAppointmentsForDay = (state, day) => {
   const result = [];
   const filteredDay = state.days.filter(item => item.name === day);
-  if (filteredDay[0]) {
-    filteredDay[0].appointments.map(id => {
-      result.push(state.appointments[id])
-    });
+  if (!filteredDay[0]) {
+    return [];
   }
+  filteredDay[0].appointments.forEach(id => {
+    result.push(state.appointments[id])
+  });
+
   return result;
 }
 
 //get interview information if its not null 
-export function getInterview(state, interview) {
+export const getInterview = (state, interview) => {
   let newObj = {};
   if (interview == null) {
     return null;
@@ -26,13 +28,26 @@ export function getInterview(state, interview) {
 }
 
 //get Interviewers for each day
-export function getInterviewersForDay(state, day) {
+export const getInterviewersForDay = (state, day) => {
   const result = [];
   const filteredDay = state.days.filter(item => item.name === day);
-  if (filteredDay[0]) {
-    filteredDay[0].interviewers.map(id => {
-      result.push(state.interviewers[id])
-    });
+  if (!filteredDay[0]) {
+    return [];
   }
+  filteredDay[0].interviewers.forEach(id => {
+    result.push(state.interviewers[id]);
+  });
   return result;
 }
+
+export const setSpots = prev => {
+  const days = prev.days.map(day => {
+    const spots = day.appointments.filter(appId => {
+      return prev.appointments[appId].interview === null;
+    });
+    // set spots with new spots remaining
+    day.spots = spots.length;
+    return day;
+  });
+  return days;
+};
